41ba00f4e259ad1dc99c9adb430d68175786de66
rexthonyy <rexthonyy@gmail.com>
2021-04-18 08:38:27 +0100

    Created new css files derived from rex.css to have a modular design, and to have more control over the font-size on mobile

--- js/pricing.js
+++ js/pricing.js
@@ -2,43 +2,43 @@ const pricingTable =
 [
 {
 	pageviews: "10K",
-	price_monthly: "$2 / mo",
-	price_yearly: "$1 / mo"
+	price_monthly: "$3 / mo",
+	price_yearly: "$2 / mo"
 },
 {
 	pageviews: "100K",
-	price_monthly: "$6 / mo",
-	price_yearly: "$3 / mo"
+	price_monthly: "$9 / mo",
+	price_yearly: "$6 / mo"
 },
 {
 	pageviews: "200K",
-	price_monthly: "$10 / mo",
-	price_yearly: "$5 / mo"
+	price_monthly: "$15 / mo",
+	price_yearly: "$10 / mo"
 },
 {
 	pageviews: "500K",
-	price_monthly: "$14 / mo",
-	price_yearly: "$7 / mo"
+	price_monthly: "$24 / mo",
+	price_yearly: "$16 / mo"
 },
 {
 	pageviews: "1m",
-	price_monthly: "$24 / mo",
-	price_yearly: "$12 / mo"
+	price_monthly: "$42 / mo",
+	price_yearly: "$28 / mo"
 },
 {
 	pageviews: "2m",
-	price_monthly: "$36 / mo",
-	price_yearly: "$18 / mo"
+	price_monthly: "$63 / mo",
+	price_yearly: "$42 / mo"
 },
 {
 	pageviews: "5m",
-	price_monthly: "$50 / mo",
-	price_yearly: "$25 / mo"
+	price_monthly: "$90 / mo",
+	price_yearly: "$60 / mo"
 },
 {
 	pageviews: "10m",
-	price_monthly: "$76 / mo",
-	price_yearly: "$32 / mo"
+	price_monthly: "$135 / mo",
+	price_yearly: "$90 / mo"
 },
 {
 	pageviews: "10m+",
